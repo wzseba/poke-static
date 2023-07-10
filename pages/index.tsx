47abe72f1adf66a98a90fe9da2895c12d3d745e4
wzseba <wzseba@gmail.com>
2023-07-09 23:24:43 -0300

    se agrego funcion staticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,46 @@
+import { FC } from "react";
 
+import { pokeApi } from "@/api";
+import { LayoutApp } from "@/components/layouts";
+import { PokemonListResponse, SmallPokemon } from "@/interfaces";
 
-export default function Home() {
+interface Props{
+  pokemons: SmallPokemon[]
+}
+
+const Home: FC<Props> = ({pokemons})=> {
+
+  
   return (
-    <>
-      <h1>hola mundo</h1>
-    </>
+    <LayoutApp title="Listado de Pokemones">
+      <ul>
+        {
+          pokemons.map(({id, name})=>(
+            <li key={id}>
+              #{id} - {name}
+            </li>
+          ))
+        }
+      </ul>
+    </LayoutApp>
   )
+}
+export default Home;
+
+export async function getStaticProps() {
+  // "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
+
+ const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+
+  const pokemons: SmallPokemon[] = data.results.map((poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i +1}.svg`
+  }))
+
+  return {
+    props: {
+      pokemons
+    },
+  }
 }
