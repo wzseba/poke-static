0102fcefb799b1ce0613291b2715b1c6b10ffd15
wzseba <wzseba@gmail.com>
2023-07-09 23:21:59 -0300

    se agrego configuracion de url externa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/PokeAPI/sprites/master/sprites/pokemon/132.png'
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
