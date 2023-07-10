623a6d6bd9d9e52eef68a6ad405a583b9f909b39
wzseba <wzseba@gmail.com>
2023-07-09 23:28:10 -0300

    se agrego componente image de next

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,30 @@
+import { Spacer, Text, useTheme } from "@nextui-org/react"
+import Image from "next/image";
+
+
+export const Navbar = () => {
+   const {theme} = useTheme();
+  return (
+    <div style={{
+        display: 'flex',
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'start',
+        padding: '0 2rem',
+        backgroundColor: theme?.colors.gray100.value
+    }}>
+        <Image
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+            alt="icono de la app"
+            width={70}
+            height={70}
+            priority={true}
+        />
+        <Text color="white" h2>P</Text>
+        <Text color="white" h3>okemon</Text>
+        <Spacer css={{flex: 1}}/>
+        <Text color="white">Favoritos</Text>
+    </div>
+  )
+}
