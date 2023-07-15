5b02f47351dc539ca1726b5e215ceca9db5561fa
wzseba <wzseba@gmail.com>
2023-07-14 22:27:25 -0300

    add: new page favorite

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import { LayoutApp } from "@/components/layouts"
+
+
+const FavoritesPage = () => {
+  return (
+    <LayoutApp title="Pokemons - Favoritos">
+        <h1>Favoritos</h1>
+    </LayoutApp>
+  )
+}
+
+export default FavoritesPage
